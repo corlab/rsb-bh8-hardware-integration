948a83044e8262d88532975f03018909b91eaf3d
xwavex <xwavedw@gmail.com>
2018-12-19 12:03:27 +0100

    Created basic structure

---
+++ src/rsb-bh8-interface.cpp
@@ -0,0 +1,35 @@
+#include "rsb-bh8-interface.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // if (argc != 2)
+    // {
+    //     cerr << "usage: " << argv[0] << " NICKNAME" << endl;
+    //     return EXIT_FAILURE;
+    // }
+    // string nick = argv[1];
+    // // Insert creation of sending and receiving participants
+    // rsb::Factory &factory = ...
+
+    //     rsb::Informer<string>::Ptr informer = ... rsb::ListenerPtr listener = ... listener->addFilter(rsb::filter::FilterPtr(new rsb::filter::OriginFilter(informer->getId(), true)));
+    // // Add a handler
+    // listener->addHandler(...);
+
+    // while (true)
+    // {
+    //     cout << "> ";
+    //     cout.flush();
+    //     boost::shared_ptr<string> message(new string());
+    //     getline(cin, *message);
+    //     if (*message == "/quit")
+    //     {
+    //         break;
+    //     }
+    //     // Send the message
+    //     informer...
+    // }
+
+    return EXIT_SUCCESS;
+}
---
+++ src/rsb-bh8-interface.hpp
@@ -0,0 +1,7 @@
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+#include <rsb/Event.h>
+#include <rsb/Factory.h>
+#include <rsb/Handler.h>
+#include <rsb/filter/OriginFilter.h>
