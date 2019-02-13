e4a2ea193005f8dd6c6ab2f9a5576a8096e195c5
sebschne <sebschne@techfak.uni-bielefeld.de>
2019-02-13 11:15:15 +0100

    code format and boost logging included

--- src/rsb-bh8-interface.cpp
+++ src/rsb-bh8-interface.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <boost/log/trivial.hpp>
 #include <string.h>
 #include <deque>
 #include "BHand.h"
@@ -55,8 +55,8 @@ const double cpr = cts / (2. * M_PI); // counts per radian
 
 void Error()
 {
-    printf("ERROR: %d\n%s\n", result, bh.ErrorMessage(result));
-    exit(0);
+	printf("ERROR: %d\n%s\n", result, bh.ErrorMessage(result));
+	exit(0);
 }
 
 ///////////////////////////////////////////////////////////
@@ -64,32 +64,32 @@ void Error()
 ///////////////////////////////////////////////////////////
 void Initialize()
 {
-    // Set hardware description before initialization
-    // we have a BH8-282, which is identical to the BH8-280 except that the base is smaller
-    // the library only knows the older BH8-280 but seems to work fine
-    int hwIndex = BHandHardware::getBHandHardwareIndex("BH8-280");
-	    if (hwIndex < 0)
-    {
-        printf("\n\nThe API has not been compiled to include target hand.\n");
-        Error();
-    }
-    printf("hwIndex 1: %d\n", hwIndex);
-    bh.setHardwareDesc(hwIndex);
-    printf("hwIndex 2: %d\n", hwIndex);
-    bool use280Config = (strcmp(bh.getHardwareDesc()->getModelNumber(), "BH8-280") == 0);
-    if (!use280Config)
-    {
-        printf("No config found\n");
-    }
+	// Set hardware description before initialization
+	// we have a BH8-282, which is identical to the BH8-280 except that the base is smaller
+	// the library only knows the older BH8-280 but seems to work fine
+	int hwIndex = BHandHardware::getBHandHardwareIndex("BH8-280");
+	if (hwIndex < 0)
+	{
+		printf("\n\nThe API has not been compiled to include target hand.\n");
+		Error();
+	}
+	printf("hwIndex 1: %d\n", hwIndex);
+	bh.setHardwareDesc(hwIndex);
+	printf("hwIndex 2: %d\n", hwIndex);
+	bool use280Config = (strcmp(bh.getHardwareDesc()->getModelNumber(), "BH8-280") == 0);
+	if (!use280Config)
+	{
+		printf("No config found\n");
+	}
 
-    if (result = handInitWithMenu(&bh, "/dev/pcanusb32"))
-        Error();
+	if (result = handInitWithMenu(&bh, "/dev/pcanusb32"))
+		Error();
 
-    printf("Initialization...");
-    if (result = bh.InitHand(""))
-        Error();
-    else
-        printf(" Done\n");
+	BOOST_LOG_TRIVIAL(info) << "Initialization...";
+	if (result = bh.InitHand(""))
+		Error();
+	else
+		BOOST_LOG_TRIVIAL(info) << " Done\n";
 }
 
 ///////////////////////////////////////////////////////////
@@ -97,50 +97,50 @@ void Initialize()
 ///////////////////////////////////////////////////////////
 void PrepareRealTime()
 {
-    // initialize everything in velocity control mode
-    // aslo sets which sensors are read out each cycle
-    // see API/BHandSupervisoryRealTime.cpp
-    if (result = bh.RTSetFlags("SG", 1, 3, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0))
-        Error();
-    sprintf(buf, "%s", "");
+	// initialize everything in velocity control mode
+	// aslo sets which sensors are read out each cycle
+	// see API/BHandSupervisoryRealTime.cpp
+	if (result = bh.RTSetFlags("SG", 1, 3, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0))
+		Error();
+	sprintf(buf, "%s", "");
 
-    // set spread to position mode
-    // teachIn is not possible with the spread in velocity mode as it is based on a PID position controller on-board
-    bh.Set("S", "LCV", 0); // LCV (loop control verlocity) false
-    bh.Set("S", "LCP", 1); // LCP (loop control position) true
+	// set spread to position mode
+	// teachIn is not possible with the spread in velocity mode as it is based on a PID position controller on-board
+	bh.Set("S", "LCV", 0); // LCV (loop control verlocity) false
+	bh.Set("S", "LCP", 1); // LCP (loop control position) true
 
-    // "LCT" is the equivalent call for torques, see API/BHandBH8_280.cpp for a list
+	// "LCT" is the equivalent call for torques, see API/BHandBH8_280.cpp for a list
 }
 
 
 int After()
 {
 
-    printf("DEACTIVATING THE HAND");	
-    if (result = bh.Command("GO")) // open all fingers
-        Error();                   //print error message
-                                   // this just checks for the key press and could be removed
-    if (UnbufferedGetChar() != EOF)
-        return 1;
+	printf("DEACTIVATING THE HAND");	
+	if (result = bh.Command("GO")) // open all fingers
+		Error();                   //print error message
+	// this just checks for the key press and could be removed
+	if (UnbufferedGetChar() != EOF)
+		return 1;
 
-    if (result = bh.Command("T")) // terminate all motors
-        Error();
-    if (UnbufferedGetChar() != EOF)
-        return 1;
-    DELAY(1);
+	if (result = bh.Command("T")) // terminate all motors
+		Error();
+	if (UnbufferedGetChar() != EOF)
+		return 1;
+	DELAY(1);
 
-    return 0;
+	return 0;
 }
 
 void closeGrasp(){
 
 	char motor[4] = "123";
-//	bh.Close(motor);
+	//	bh.Close(motor);
 	result = bh.Command("GC");
 	std::cout << result << std::endl;
 	if (result)
 		Error();
-//	return 0;
+	//	return 0;
 }
 
 void openGrasp(){
@@ -148,96 +148,92 @@ void openGrasp(){
 	if (result)
 		Error();
 	std::cout << result << std::endl;
-//	return 0;
+	//	return 0;
 }
 
 void receiveCommands(boost::shared_ptr<std::string> e){
 	std::cout << "RECEIVED EVENT" << *e << std::endl;
 	if (*e == "open"){
-		printf("----- opening -----\n");
+		BOOST_LOG_TRIVIAL(info) << "----- opening -----\n";
 		openGrasp();}
 	else if (*e =="close"){
-		printf("----- closing hands -----\n");
+		BOOST_LOG_TRIVIAL(info) << "----- closing hands -----\n";
 		closeGrasp();}
-	else if (*e == "deactivate"){
-		printf("----- deactivate ----- \n");
+	else if (*e == "deactivate"){ 
+		BOOST_LOG_TRIVIAL(info) << "----- deactivate ----- \n";
 		After();}
 
 }
 
-	
+
 
 int main(int argc, char *argv[])
 {
 
-    printf("\n\n\r\t\tInitializing Software...\n");
-    Initialize(); // initializes the hardware
+	BOOST_LOG_TRIVIAL(info) << "\n\n\r\t\tInitializing Software...\n";
+	Initialize(); // initializes the hardware
 
 	PrepareRealTime();
 
 	bh.RTStart("GS", BHMotorTorqueLimitProtect);
-    bh.RTUpdate();
-//    bh.RTAbort();
-	   boost::shared_ptr< rsb::converter::ProtocolBufferConverter<rst::dynamics::Wrench> >
-        converter(new rsb::converter::ProtocolBufferConverter<rst::dynamics::Wrench>());
-    rsb::converter::converterRepository<std::string>()->registerConverter(converter);
+	bh.RTUpdate();
+	//    bh.RTAbort();
+	boost::shared_ptr< rsb::converter::ProtocolBufferConverter<rst::dynamics::Wrench> >
+		converter(new rsb::converter::ProtocolBufferConverter<rst::dynamics::Wrench>());
+	rsb::converter::converterRepository<std::string>()->registerConverter(converter);
 
-        boost::shared_ptr < rst::dynamics::Wrench > wrench = boost::shared_ptr< rst::dynamics::Wrench > (new rst::dynamics::Wrench());
+	boost::shared_ptr < rst::dynamics::Wrench > wrench = boost::shared_ptr< rst::dynamics::Wrench > (new rst::dynamics::Wrench());
 
 
 
 	rsc::misc::initSignalWaiter();
-	
+
 	Factory& factory = getFactory();
 	Scope scope((argc >1) ? argv[1] : "/bhand/listener");
 	std::string informer_scope = (argc >2) ? argv[2] : "/bhand/informer/wrench";
 	ListenerPtr listener = factory.createListener(scope);
-	printf("Adding handlers ...\n");
+	 BOOST_LOG_TRIVIAL(info) << "Adding handlers ...\n";
 	listener->addHandler(HandlerPtr(new DataFunctionHandler<std::string> (&receiveCommands)));
-	printf("Finished adding handlers\n");
-	
-	Informer<rst::dynamics::Wrench>::Ptr informer = factory.createInformer<rst::dynamics::Wrench>(informer_scope);
-    bh.RTTareFT(); // To calibrate
-	printf("Finished calibration\n");
+	BOOST_LOG_TRIVIAL(info) << "Finished adding handlers\n";
 
-	
-	printf("Publishing wrench at: ");
+	Informer<rst::dynamics::Wrench>::Ptr informer = factory.createInformer<rst::dynamics::Wrench>(informer_scope);
+	bh.RTTareFT(); // To calibrate
+	BOOST_LOG_TRIVIAL(info) << "Finished calibration\n";
+
+
+	BOOST_LOG_TRIVIAL(info)<<"Publishing ft-data on " << informer_scope;
 	rst::dynamics::Forces* forces = wrench->mutable_forces();
 
 	rst::dynamics::Torques* torques = wrench->mutable_torques();
 	double f[3]; //force from ForceTorque sensor
-	printf("Created array for force sensor data \n");
-	double t[3]; // torque from ForceTorque sensor
-	printf("Created array for torue sensor data \n");
-	double a[3]; // acceleration from ..
-	printf("Created array for acceleration sensor data\n");
-	unsigned int microseconds = 1500000;
+	double t[3]; //torque from ForceTorque sensor
+	double a[3]; //acceleration from ..
+	unsigned int microseconds = 1500000; //sleep needed for work in vm
+	BOOST_LOG_TRIVIAL(info) << "Press Enter and ctrl+c to stop the program";
 
 	while (UnbufferedGetChar() == EOF)
-    {
-	std::cout << "reading force torque: "  << std::endl;
-	bh.RTGetFT(f,t);
-	std::cout << " finished reading force torque: "  << std::endl;
-//	printf("Force: %7.3f %7.3f %7.3f \n", f[0], f[1], f[2]);
-//	printf("Torque: %7.3f %7.3f %7.3f \n", t[0], t[1], t[2]);
-//      bh.RTGetA(a);     // get acceleration from the FT sensor
-//      printf("Acceleration %7.3f %7.3f %7.3f\n", a[0], a[1], a[2]);
-	usleep(microseconds);
-	forces->set_x(f[0]);
-	forces->set_y(f[1]);
-	forces->set_z(f[2]);
-	
-	torques->set_a(t[0]);
-	torques->set_b(t[1]);
-	torques->set_c(t[2]);
-	informer->publish(wrench);	
+	{
+		bh.RTGetFT(f,t);
+		//printf("Force: %7.3f %7.3f %7.3f \n", f[0], f[1], f[2]);
+		//printf("Torque: %7.3f %7.3f %7.3f \n", t[0], t[1], t[2]);
+		//bh.RTGetA(a);     // get acceleration from the FT sensor
+		//printf("Acceleration %7.3f %7.3f %7.3f\n", a[0], a[1], a[2]);
+		usleep(microseconds);
+		forces->set_x(f[0]);
+		forces->set_y(f[1]);
+		forces->set_z(f[2]);
+
+		torques->set_a(t[0]);
+		torques->set_b(t[1]);
+		torques->set_c(t[2]);
+		informer->publish(wrench);	
 	}		
 
-	
 
-	printf("Stopping program");
+
+	BOOST_LOG_TRIVIAL(info) << "Stopping program";
 	After();
 	return rsc::misc::suggestedExitCode(rsc::misc::waitForSignal());
 
-	
+
 }
