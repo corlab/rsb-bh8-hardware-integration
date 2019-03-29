26c40a83c51a3c539069945ea0f5a97dd48fe267
xwavex <xwavedw@gmail.com>
2019-03-29 14:23:42 +0100

    fixed plotting

--- python/liveFTplot.py
+++ python/liveFTplot.py
@@ -5,9 +5,11 @@ from matplotlib import style
 import time
 import logging
 import rsb
+import rsb.converter
 import rst
 import rstsandbox
 from rst.dynamics.Wrench_pb2 import Wrench
+import numpy as np
 
 # rsb0.16 send '"0.3,0.3,0.3"' 'socket:/debug/ft'
 
@@ -15,9 +17,9 @@ style.use('fivethirtyeight')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, aspect='equal', projection='3d')
-ax1.set_xlim3d(-1, 1)
-ax1.set_ylim3d(-1, 1)
-ax1.set_zlim3d(-1, 1)
+ax1.set_xlim3d(-10, 10)
+ax1.set_ylim3d(-10, 10)
+ax1.set_zlim3d(-10, 10)
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 ax1.set_zlabel('z')
@@ -29,23 +31,26 @@ zs = [0.0]
 
 def handle(event):
     # print("Received event: %s" % event)
-    # global xs, ys, zs
-    # xs = [0.0]
-    # ys = [0.0]
-    # zs = [0.0]
+    global xs, ys, zs
+    xs = [0.0]
+    ys = [0.0]
+    zs = [0.0]
+    arr = np.array([float(event.data.forces.x), float(event.data.forces.y), float(event.data.forces.z)])
+
     # x, y, z = event.data.split(',')
-    xs.append(float(event.data.forces.x))
-    ys.append(float(event.data.forces.y))
-    zs.append(float(event.data.forces.z))
+    xs.append(arr[0])
+    ys.append(arr[1])
+    zs.append(arr[2])
+    # print(event.metaData.deliverTime - event.metaData.createTime)
 
 
 def animate(i):
     ax1.clear()
     global xs, ys, zs
     ax1.plot(xs, ys, zs)
-    ax1.set_xlim3d(-1, 1)
-    ax1.set_ylim3d(-1, 1)
-    ax1.set_zlim3d(-1, 1)
+    ax1.set_xlim3d(-10, 10)
+    ax1.set_ylim3d(-10, 10)
+    ax1.set_zlim3d(-10, 10)
     ax1.set_xlabel('x')
     ax1.set_ylabel('y')
     ax1.set_zlabel('z')
@@ -57,10 +62,13 @@ if __name__ == '__main__':
     # Pacify logger.
     logging.basicConfig()
 
+    converter = rsb.converter.ProtocolBufferConverter(messageClass=Wrench)
+    rsb.converter.registerGlobalConverter(converter)
+
     # Create a listener on the specified scope. The listener will
     # dispatch all received events asynchronously to all registered
     # handlers.
-    with rsb.createListener("/debug/ft") as listener:
+    with rsb.createListener("/bhand/left/wrenches") as listener:
 
         # Add a handler to handle received events. Handlers are callable
         # objects with the received event as the single argument.
@@ -69,5 +77,5 @@ if __name__ == '__main__':
         # Wait for events; clean up when interrupted.
         # while True:
         #     time.sleep(1)
-        ani = animation.FuncAnimation(fig, animate, interval=1)
+        ani = animation.FuncAnimation(fig, animate, interval=180)
         plt.show()
